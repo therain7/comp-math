340bdeca12ba4a96aa26228221c542966f06bf83
Andrei <therain.i@yahoo.com>
2024-05-15 06:22:33 +0300

    feat(CLI): ability to choose svd method

--- task3/imsvd.py
+++ task3/imsvd.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 
 from imsvd.rgbimg import RGBImage
-from imsvd.svd import numpy_svd
+from imsvd.svd import numpy_svd, power_svd
 from imsvd.svdimg import SVDImage
 
 
@@ -18,25 +18,39 @@ def main() -> None:
     parser.add_argument("-o", "--output", help="output file path", required=True)
     parser.add_argument("-r", "--ratio", help="compression ratio", type=float)
     parser.add_argument(
+        "-m", "--method", help="svd computation method", choices=["numpy", "power"]
+    )
+    parser.add_argument(
         "-d", "--decompress", action="store_true", help="set to decompress image"
     )
 
     args = parser.parse_args()
-    if not args.decompress and (args.ratio is None):
-        parser.error("-r/--ratio is required")
+    if not args.decompress and (args.ratio is None or args.method is None):
+        parser.error(
+            "some of the following required parameters are not present: "
+            "-r/--ratio, -m/--method"
+        )
 
     if args.decompress:
         decompress(Path(args.input), Path(args.output))
     else:
-        compress(Path(args.input), Path(args.output), args.ratio)
+        compress(Path(args.input), Path(args.output), args.ratio, args.method)
 
 
-def compress(inp: Path, out: Path, ratio: float) -> None:
+def compress(inp: Path, out: Path, ratio: float, method: str) -> None:
     raw_size = os.path.getsize(inp)
     desired_size = math.floor(raw_size / ratio)
 
+    match method:
+        case "numpy":
+            svd = numpy_svd
+        case "power":
+            svd = power_svd
+        case _:
+            raise ValueError("Unknown method")
+
     img = RGBImage.open(inp)
-    SVDImage.of_rgb_image(img, desired_size, svd=numpy_svd).save(out)
+    SVDImage.of_rgb_image(img, desired_size, svd).save(out)
 
 
 def decompress(inp: Path, out: Path) -> None:
